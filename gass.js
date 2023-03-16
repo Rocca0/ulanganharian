318103a57826ca54bbc6816755f06fe3afaaabb5
Arly ALfaridzy Loka Prima <94875709+Rocca0@users.noreply.github.com>
2023-03-16 11:30:21 +0700

    Add files via upload

---
+++ gass.js
@@ -0,0 +1,70 @@
+//no.1
+function cetakFuntion(a){
+    return `Hallo nama saya` + a
+}
+console.log(cetakFuntion("DICKY"))   
+
+// //no.2
+// function penjumlahan(a,b){
+//     return a + b;
+// }
+// let angka1 = 20;
+// let angka2 = 7;
+// let output = penjumlahan (angka1,angka2)
+
+// console.log(output);
+
+// //no.3
+// let hello = hello((hello) => "loka");
+
+// return "hello"    
+// console.log(hello)
+
+
+
+// No.4
+// let obj = {
+//     nama : "John",
+//     umur : 22,
+//     bahasa : "Indonesia",
+//      fullName: function(){
+//         console.log(`nama ${this.nama} ${this.umur} ${this.bahasa}`)
+//     }
+// }
+// obj.fullName
+
+// No.5
+// let arr = ["john Doe","laki-laki","baca buku",1992]
+// let objek = {
+//     nama:arr(0),
+//     gender:arr(1),
+//     hobi:arr(2),
+//     TTL:arr(3),
+// }
+// console.log(objek);
+
+// no.6
+// let buah = [{
+// nama: "nanas",
+// biji: "tidak ada",
+// harga: 9000,
+// },
+// {
+//    nama : "jeruk",
+//    biji : "ada",
+//    harga : 5000,
+// },
+// {
+//     nama : "semangka",
+//     biji : "tidak ada",
+//     harga : 15000,
+//  },
+//  {
+//     nama : "pisang",
+//     biji : "ada",
+//     harga : 8000,
+//  },
+
+// ]
+
+
